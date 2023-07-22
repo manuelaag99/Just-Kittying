8fea747fa440aaa9e8b93225f6387f1b1ee4c41c
Manuel Alanis <manuelaag99@gmail.com>
2023-07-22 14:33:50 -0600

    functional profile settings page

--- client side/src/Components/InputForForm.jsx
+++ client side/src/Components/InputForForm.jsx
@@ -2,15 +2,26 @@ import React, { useEffect, useReducer } from "react";
 
 import CancelIcon from '@mui/icons-material/Cancel';
 
-const initialState = { value: "", isValid: false };
+import { isTextAnEmail, isTextAPassword, minLengthText, nonEmptyText } from "../CheckValidity";
+
 
 function reducer (state, action) {
     switch (action.type) {
         case "change":
-            
+            let checkValidity;
+            if (action.placeholder === "Enter your e-mail") {
+                checkValidity = isTextAnEmail(action.val);
+            } else if (action.placeholder === "Create a username") {
+                checkValidity = minLengthText(action.val, 6);
+            } else if (action.placeholder === "Create a password") {
+                checkValidity = isTextAPassword(action.val);
+            } else {
+                checkValidity = nonEmptyText(action.val);
+            }
             return {
                 ...state,
-                value: action.value
+                value: action.value,
+                isValid: checkValidity
             };
         case "focus":
             return {
@@ -28,7 +39,9 @@ function reducer (state, action) {
     }
   }
 
-export default function InputForForm ({ individualInputAction, inputClassnames, inputName, inputPlaceholder, inputType, inputValue, isInSettingsPage, isSelect, labelClassnames, labelText }) {
+export default function InputForForm ({ individualInputAction, inputClassnames, inputName, inputPlaceholder, inputType, inputValue, isInSettingsPage, isSelect, labelClassnames, labelText, optionsForSelect }) {
+    const initialState = { value: inputValue, isValid: false };
+
     const [individualInputState, dispatch] = useReducer(reducer, initialState);
     const {value, isValid} = individualInputState
 
@@ -37,7 +50,7 @@ export default function InputForForm ({ individualInputAction, inputClassnames, 
     }
 
     function inputChangeHandle (e) {
-        dispatch({ type: "change", value: e.target.value });
+        dispatch({ type: "change", value: e.target.value, placeholder: e.target.placeholder });
     }
 
     function inputFocusHandle () {
@@ -46,18 +59,23 @@ export default function InputForForm ({ individualInputAction, inputClassnames, 
 
     useEffect(() => individualInputAction(value, isValid, inputName), [value, isValid, inputName]);
 
-    console.log(individualInputState.value)
 
     return (
-        <div className="flex flex-row w-full h-fit items-center mb-3 pr-2 pl-3 s">
+        <div className="flex flex-row w-full h-fit items-center mb-3 pr-2 pl-3 ">
             {isInSettingsPage && <label className={labelClassnames} htmlFor="">{labelText}</label>}
             <div className="flex flex-col w-6/10 px-2">
 
-                {!isSelect && <input autoComplete="off" className={inputClassnames} name={inputName} onBlur={inputBlurHandle} onChange={inputChangeHandle} onFocus={inputFocusHandle} placeholder={inputPlaceholder} type={inputType} value={inputValue} />}
+                {!isSelect && <input autoComplete="off" className={inputClassnames} name={inputName} onBlur={inputBlurHandle} onChange={inputChangeHandle} onFocus={inputFocusHandle} placeholder={inputPlaceholder} type={inputType} value={value} />}
                 {isInSettingsPage && (inputName === "displayname" || inputName === "username") && <div className="flex flex-row items-center">
                     {(individualInputState.value === "" && individualInputState.isActive && individualInputState.isTouched) && <CancelIcon className="text-red-700" fontSize="small"/>}
                     {(individualInputState.value === "" && individualInputState.isActive && individualInputState.isTouched) && <p className="text-red-700 text-errorFont pl-2">This field can't be empty</p>}
                 </div>}
+
+                {isSelect && <select className={inputClassnames} name={inputName} id={inputName} onChange={inputChangeHandle} value={value} >
+                    {optionsForSelect.map((option, index) => {
+                        return <option key={index} value={option.value}>{option.text}</option>
+                    })}
+                </select>}
 
             </div>
         </div>
--- client side/src/Pages/ProfileSettingsPage.jsx
+++ client side/src/Pages/ProfileSettingsPage.jsx
@@ -53,19 +53,6 @@ export default function ProfileSettingsPage () {
     }, []);
 
 
-    const [loading, setLoading] = useState(true)
-    const [profileFormData, setProfileFormData] = useState({
-        inputs: {
-            displayname: { value: "", isValid: false },
-            username: { value: "", isValid: false },
-            shortbio: { value: "", isValid: false },
-            accountprivacy: { value: "", isValid: false },
-            feedpreference: { value: "", isValid: false }
-        },
-        isFormValid: false
-    });
-
-
     const [formState, setFormState] = useState({
         inputs: {
             displayname: { value: "", isValid: false },
@@ -84,46 +71,8 @@ export default function ProfileSettingsPage () {
         dispatch({ type: "formChange", inputName: inputName, value: value, isValid: isValid })
     }, [dispatch])
 
-
-    useEffect(() => {
-        if (userInfo) {
-            setFormState({
-                inputs: {
-                    displayname: { value: userInfo.display_name, isValid: true },
-                    username: { value: userInfo.username, isValid: true },
-                    shortbio: { value: userInfo.short_bio, isValid: true },
-                    accountprivacy: { value: userInfo.account_privacy, isValid: true },
-                    feedpreference: { value: userInfo.feed_preference, isValid: true }
-                }, isFormValid: true
-            })
-        }
-    }, [userInfo]);
-
-    const [formErrors, setFormError] = useState(false);
-    useEffect(() => {
-        if ((profileFormData.displayname === "") || (profileFormData.username === "")) {
-            console.log("error")
-            setFormError(true)
-        } else {
-            setFormError(false)
-        }
-    }, [profileFormData])
-
-
-    function changeHandle (e) {
-        const { name, value } = e.target;
-        setProfileFormData((prevState) => ({
-          ...prevState, [name]: value,
-        }));
-        if (value === "") {
-            setFormError(true)
-        } else {
-            setFormError(false)
-        }
-    };
-
     function DoneButtonHandle () {
-        if (!formErrors) {
+        if (stateOfForm.isFormValid) {
             navigate("/")
         }
     }
@@ -132,7 +81,7 @@ export default function ProfileSettingsPage () {
     console.log(stateOfForm)
 
     if (!userInfo) {
-        return (<LoadingSpinner open={loading} />)
+        return (<LoadingSpinner open={true} />)
     } else {
         return (
             <div className="flex flex-col justify-center items-center w-full h-fit">
@@ -144,7 +93,7 @@ export default function ProfileSettingsPage () {
                     <div className="flex flex-row items-center justify-center w-2/10 pt-0.5">
                         Settings
                     </div>
-                    <button disabled={formErrors} className="flex flex-row items-centerw-fit pl-3 pr-4 py-2 bg-var-1 hover:bg-var-2 duration-100 active:text-black disabled:text-var-2 " onClick={DoneButtonHandle}>
+                    <button disabled={!stateOfForm.isFormValid} className="flex flex-row items-centerw-fit pl-3 pr-4 py-2 bg-var-1 hover:bg-var-2 duration-100 active:text-black disabled:text-var-2 " onClick={DoneButtonHandle}>
                         <CheckIcon />
                         <p className="pl-1 pt-0.5">Done</p>
                     </button>
@@ -156,30 +105,15 @@ export default function ProfileSettingsPage () {
                     </div>
                     <div className="flex flex-col w-full h-fit border-var-2 border-solid border-2 mt-0 pt-3">
                         
-                        <InputForForm individualInputAction={formHandler} inputClassnames="w-full pt-1 outline-none " inputName="displayname" inputPlaceholder="Enter your display name..." inputType="text" inputValue={formState.inputs.displayname.value} isInSettingsPage={true} isSelect={false} labelClassnames="w-4/10 pr-2" labelText="Display name: " />
+                        <InputForForm individualInputAction={formHandler} inputClassnames="w-full pt-1 outline-none " inputName="displayname" inputPlaceholder="Enter your display name..." inputType="text" inputValue={userInfo.display_name || ""} isInSettingsPage={true} isSelect={false} labelClassnames="w-4/10 pr-2" labelText="Display name: " />
 
-                        <InputForForm individualInputAction={formHandler} inputClassnames="w-full pt-1 outline-none " inputName="username" inputPlaceholder="Enter a user name..." inputType="text" inputValue={formState.inputs.username.value} isInSettingsPage={true} isSelect={false} labelClassnames="w-4/10 pr-2" labelText="Username: " />
+                        <InputForForm individualInputAction={formHandler} inputClassnames="w-full pt-1 outline-none " inputName="username" inputPlaceholder="Enter a user name..." inputType="text" inputValue={userInfo.username || ""} isInSettingsPage={true} isSelect={false} labelClassnames="w-4/10 pr-2" labelText="Username: " />
                         
-                        <InputForForm individualInputAction={formHandler} inputClassnames="w-full pt-1 outline-none" inputName="shortbio" inputPlaceholder="Enter a short bio..." inputType="text" inputValue={formState.inputs.shortbio.value} isInSettingsPage={true} isSelect={false} labelClassnames="w-4/10 pr-2" labelText="Short bio: " />
+                        <InputForForm individualInputAction={formHandler} inputClassnames="w-full pt-1 outline-none" inputName="shortbio" inputPlaceholder="Enter a short bio..." inputType="text" inputValue={userInfo.short_bio || ""} isInSettingsPage={true} isSelect={false} labelClassnames="w-4/10 pr-2" labelText="Short bio: " />
 
-                        {/* <InputForForm individualInputAction={formHandler} inputClassnames="w-6/10 px-2 pt-1 outline-none" inputName="accountprivacy" inputValue={formState.inputs.accountprivacy.value} isInSettingsPage={true} isSelect={true} labelClassnames="w-4/10 pr-2" labelText="Account privacy: " /> */}
+                        <InputForForm optionsForSelect={[{ value: "private", text: "Private"}, { value: "public", text: "Public"}]} individualInputAction={formHandler} inputClassnames="w-full pt-1 outline-none" inputName="accountprivacy" inputValue={userInfo.account_privacy} isInSettingsPage={true} isSelect={true} labelClassnames="w-4/10 pr-2" labelText="Account privacy: " />
                         
-                        <div className="flex flex-row w-full h-fit items-center mb-3 pr-2 pl-3">
-                            <label className="w-4/10 pr-2" htmlFor="accountprivacy"> Account privacy:  </label>
-                            <select className="w-6/10 px-2 pt-1 outline-none" name="accountprivacy" id="accountprivacy" onChange={changeHandle} value={profileFormData.accountprivacy} >
-                                <option value="private">Private</option>
-                                <option value="public">Public</option>
-                            </select>
-                        </div>
-                        
-                        <div className="flex flex-row w-full h-fit items-center mb-3 pr-2 pl-3">
-                        <label className="w-4/10 pr-2" htmlFor="feedpreference"> Feed preference:  </label>
-                            <select className="w-6/10 px-2 pt-1 outline-none" name="feedpreference" id="feedpreference" onChange={changeHandle} value={profileFormData.feedpreference} >
-                                <option value="friends">Friends only</option>
-                                <option value="all">All</option>
-                            </select>
-                        </div>
-
+                        <InputForForm optionsForSelect={[{ value: "friends", text: "Friends only"}, { value: "all", text: "All"}]} individualInputAction={formHandler} inputClassnames="w-full pt-1 outline-none" inputName="feedpreference" inputValue={userInfo.user_preference} isInSettingsPage={true} isSelect={true} labelClassnames="w-4/10 pr-2" labelText="Feed preference: " />
 
                         <div className="flex flex-row justify-between w-full h-fit pt-3 pb-2 px-3 bg-var-1 hover:bg-var-2 duration-200 cursor-pointer">
                             <p>Change password</p>
