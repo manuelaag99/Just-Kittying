a665f6405b5c32be650bba0c15b359bb780d0b55
Manuel Alanis <manuelaag99@gmail.com>
2023-09-23 09:03:06 -0600

    functioning conditional render of users in list

--- client side/src/Components/UserInList.jsx
+++ client side/src/Components/UserInList.jsx
@@ -4,6 +4,7 @@ import RoundPhoto from "./RoundPhoto";
 
 import PersonAddAlt1Icon from '@mui/icons-material/PersonAddAlt1';
 import PersonRemoveIcon from '@mui/icons-material/PersonRemove';
+import LoadingSpinner from "./Portals/LoadingSpinner";
 
 export default function UserInList ({ index, userId }) {
     console.log(userId)
@@ -23,23 +24,35 @@ export default function UserInList ({ index, userId }) {
         fetchUserInfo();
     }, [])
 
-    console.log(userInfo);
-
-    return (
-        <div key={index} className="flex flex-row w-full py-2 hover:bg-var-2 duration-200 cursor-pointer ">
+    if (!userInfo) {
+        return (
+            <div key={index} className="flex flex-row w-full py-2 hover:bg-var-2 duration-200 cursor-pointer ">
             <div className="flex flex-row justify-start w-8/10">
-                <RoundPhoto classesForRoundPhoto="flex justify-center items-center h-userProfileFriendsTabPhotoHeight aspect-square ml-2" imageAlt="friend-profile-pic" imageSource={userId.profile_pic_url || null} />
-                <div className="flex flex-col w-fit pl-4 pr-2">
-                    <div className="font-bold" >{userInfo.display_name}</div>
-                    <div className="opacity-30" >{userInfo.username}</div>
+                <div className="rounded-circular bg-gray-200 aspect-square ml-2 h-userProfileFriendsTabPhotoHeight"></div>
+                <div className="flex flex-col w-6/10 pl-4 pr-2">
+                    <div className="w-6/10 bg-gray-400 h-4 rounded-input my-1"></div>
+                    <div className="w-6/10 bg-gray-300 h-4 rounded-input my-1"></div>
                 </div>
             </div>
-            <div className="flex justify-center sm:justify-end sm:pr-4 w-1/10 items-center">
-                <PersonAddAlt1Icon className="text-black hover:text-var-4 duration-100" fontSize="small" />
-            </div>
-            <div className="flex justify-center sm:justify-end sm:pr-4 w-1/10 items-center">
-                <PersonRemoveIcon className="text-black hover:text-var-4 duration-100" fontSize="small" />
-            </div>
         </div>
-    )
+        )
+    } else {
+        return (
+            <div key={index} className="flex flex-row w-full py-2 hover:bg-var-2 duration-200 cursor-pointer ">
+                <div className="flex flex-row justify-start w-8/10">
+                    <RoundPhoto classesForRoundPhoto="flex justify-center items-center h-userProfileFriendsTabPhotoHeight aspect-square ml-2" imageAlt="friend-profile-pic" imageSource={userId.profile_pic_url || null} />
+                    <div className="flex flex-col w-fit pl-4 pr-2">
+                        <div className="font-bold" >{userInfo.display_name}</div>
+                        <div className="opacity-30" >{userInfo.username}</div>
+                    </div>
+                </div>
+                <div className="flex justify-center sm:justify-end sm:pr-4 w-1/10 items-center">
+                    <PersonAddAlt1Icon className="text-black hover:text-var-4 duration-100" fontSize="small" />
+                </div>
+                <div className="flex justify-center sm:justify-end sm:pr-4 w-1/10 items-center">
+                    <PersonRemoveIcon className="text-black hover:text-var-4 duration-100" fontSize="small" />
+                </div>
+            </div>
+        )
+    }
 }
--- client side/src/Components/UsersList.jsx
+++ client side/src/Components/UsersList.jsx
@@ -1,45 +1,8 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 
-import RoundPhoto from "./RoundPhoto";
-import { supabase } from "../supabase/client";
 import UserInList from "./UserInList";
 
 export default function ({ selectedUsersArray }) {
-    const [allUsers, setAllUsers] = useState();
-    async function fetchAllUsers () {
-		try {
-			const { data, error } = await supabase.from('jk-users').select("*");
-			if (error) console.log(error);
-			setAllUsers(data);
-		} catch (err) {
-			console.log(err);
-		}
-    }
-
-    useEffect(() => {
-        fetchAllUsers();
-    }, [])
-
-    console.log(selectedUsersArray)
-
-    // const [usersInfo, setUsersInfo] = useState();
-    // useEffect(() => {
-    //     if (allUsers) {
-    //         setUsersInfo(allUsers.map((user) => {
-    //             selectedUsersArray.map((selectedUser) => {
-    //                 console.log(selectedUser.user_1_id)
-    //                 console.log(user.user_id)
-    //                 if (selectedUser.user_1_id === user.user_id) {
-    //                     console.log("match")
-    //                     return user;
-    //                 }
-    //             })
-    //         }))
-    //     }
-    // }, [allUsers])
-
-    // console.log(usersInfo)
-    
     if (!selectedUsersArray) {
         return (
             <div className="flex justify-center mt-8">
