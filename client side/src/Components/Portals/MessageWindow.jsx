53899aadd820181bb8425f0ee4fb91cac351af58
Manuel Alanis <manuelaag99@gmail.com>
2023-09-28 21:05:33 -0600

    functional conditional rendering of confirm signup message

--- client side/src/Components/Portals/MessageWindow.jsx
+++ client side/src/Components/Portals/MessageWindow.jsx
@@ -1,16 +1,28 @@
+import CloseIcon from '@mui/icons-material/Close';
+
 export default function MessageWindow({ onClose, open, textForMessage }) {
 
     const messageWindow = (
         <div>
-            <div onClick={onClose} className="bg-black opacity-50 fixed top-0 bottom-0 w-screen h-screen z-20"></div>
-            <div className="flex flex-col fixed justify-center items-center z-50 sm:left-[25%] left-[5%] sm:w-5/10 w-9/10 h-fit bg-var-1 rounded-button  text-signInOrsignUpMob sm:text-signInOrsignUpDsk duration-500 ">
-                <p>{textForMessage}</p>
+            <div onClick={onClose} className="bg-black opacity-50 fixed top-0 bottom-0 w-screen h-screen z-40"></div>
+            <div className="flex flex-col fixed justify-center items-center z-50 top-[30%] left-[5%] sm:left-[30%] w-9/10 sm:w-4/10 h-fit bg-var-1 rounded-button shadow-2xl text-signInOrsignUpMob sm:text-signInOrsignUpDsk duration-500 ">
+                <div className="flex flex-row w-full h-fit py-2">
+                    <div className="w-9/10"></div>
+                    <button className="flex justify-center items-center w-1/10 text-black hover:text-gray-300 duration-200 " onClick={onClose}>
+                        <CloseIcon />
+                    </button>
+                </div>
+                <div className="py-5 px-5">
+                    <div>
+                        <p>ijfreiufneofjeuinfeufneinYeah</p>
+                    </div>
+                </div>
             </div>
         </div>
     )
 
     if (open) {
-        return messageWindow;
+        return createPortal(messageWindow, document.body)
     } else {
         null;
     }
--- client side/src/Components/Portals/SignInOrSignUpWindow.jsx
+++ client side/src/Components/Portals/SignInOrSignUpWindow.jsx
@@ -9,6 +9,7 @@ import GoogleIcon from '@mui/icons-material/Google';
 import InputForForm from "../InputForForm";
 import { useForm } from "../custom-hooks";
 import { supabase } from "../../supabase/client";
+import MessageWindow from "./MessageWindow";
 
 export default function SignInOrSignUpWindow({ open, onClose, switchToSignIn, switchToSignUp, textForSignInOrSignUpButton }) {
     const navigate = useNavigate();
@@ -55,7 +56,11 @@ export default function SignInOrSignUpWindow({ open, onClose, switchToSignIn, sw
                 try {
                     const { userdata, error } = await supabase.from("jk-users").insert({ user_id: data.user.id, username: stateOfForm.inputs.username.value, email: stateOfForm.inputs.username.value, feed_preference: "public", account_privacy: "all", password: stateOfForm.inputs.password.value, creation_date: new Date().toISOString(), display_name: "" })
                     if (error) console.log(error);
-                    navigate("/settings");
+                    // navigate("/settings");
+                    if (!error) {
+                        setTextForMessageWindow("Successfully created your account! Checked your e-mail and confirm your signup, then come back and sign in!");
+                        setIsMessageWindowOpen(true);
+                    }
                 } catch (err) {
                     console.log(err);
                 }
@@ -95,19 +100,24 @@ export default function SignInOrSignUpWindow({ open, onClose, switchToSignIn, sw
         }
     }
 
+    const [textForMessageWindow, setTextForMessageWindow] = useState("");
+    const [isMessageWindowOpen, setIsMessageWindowOpen] = useState(false);
+
     const signInOrSignUpWindow = (
         <div>
             <div onClick={onClose} className="bg-black opacity-50 fixed top-0 bottom-0 w-screen h-screen z-20"></div>
-            <div className={"flex flex-col fixed justify-center items-center z-50 sm:left-[25%] left-[5%] sm:w-5/10 w-9/10 h-fit bg-var-1 rounded-button  text-signInOrsignUpMob sm:text-signInOrsignUpDsk duration-500 " + (textForSignInOrSignUpButton === "Sign up" ? "top-[10%] " : "top-[15%] ")}>
+            <MessageWindow onClose={() => setIsMessageWindowOpen(false)} open={isMessageWindowOpen} textForMessage={textForMessageWindow} />
+            <div className={"flex flex-col fixed justify-center items-center z-30 sm:left-[25%] left-[5%] sm:w-5/10 w-9/10 h-fit bg-var-1 rounded-button  text-signInOrsignUpMob sm:text-signInOrsignUpDsk duration-500 " + (textForSignInOrSignUpButton === "Sign up" ? "top-[10%] " : "top-[15%] ")}>
+                
                 <div className=" flex justify-center items-center w-7/10 h-10 mt-5">
                     <img className="h-full object-cover " src="images/logo.png" alt="just-kittying-logo" />
                 </div>
                 <div className="w-8/10 h-fit mt-4">
                     
-                    {(textForSignInOrSignUpButton === "Sign up") && <InputForForm smallDivClassnames="w-full h-full py-2 mb-4 px-2.5 sm:pr-0 rounded-input border-var-2 border-2 border-solid" individualInputAction={formHandler} inputClassnames="w-85 sm:w-9/10 outline-none " inputName="username" inputPlaceholder="Create a username..." inputValidity={false} inputValue={""} isInSettingsPage={false} isPasswordField={false} isSelect={false} />}
-                    <InputForForm smallDivClassnames="w-full h-full py-2 mb-4 px-2.5 sm:pr-0 rounded-input border-var-2 border-2 border-solid" individualInputAction={formHandler} inputClassnames="w-9/10 outline-none " inputName="email" inputPlaceholder="Write in your e-mail..." inputType="text" inputValidity={false} inputValue={""} isInSettingsPage={false} isPasswordField={false} isSelect={false} />
-                    <InputForForm smallDivClassnames="w-full h-full py-2 mb-4 px-2.5 sm:pr-0 rounded-input border-var-2 border-2 border-solid" individualInputAction={formHandler} inputClassnames="w-85 sm:w-9/10 outline-none " inputName="password" inputPlaceholder="Write in your password..." inputValidity={false} inputValue={""} isInSettingsPage={false} isPasswordField={true} isSelect={false} />
-                    {(textForSignInOrSignUpButton === "Sign up") && <InputForForm smallDivClassnames="w-full h-full py-2 mb-4 px-2.5 sm:pr-0 rounded-input border-var-2 border-2 border-solid" individualInputAction={formHandler} inputClassnames="w-85 sm:w-9/10 outline-none " inputName="confirmPassword" inputPlaceholder="Confirm your password..." inputValidity={false} inputValue={""} isInSettingsPage={false} isPasswordField={true} isSelect={false} />}
+                    {(textForSignInOrSignUpButton === "Sign up") && <InputForForm smallDivClassnames="w-full h-full py-2 mb-4 px-4 sm:pr-0 rounded-input border-var-2 border-2 border-solid" individualInputAction={formHandler} inputClassnames="w-85 sm:w-9/10 outline-none " inputName="username" inputPlaceholder="Create a username..." inputValidity={false} inputValue={""} isInSettingsPage={false} isPasswordField={false} isSelect={false} />}
+                    <InputForForm smallDivClassnames="w-full h-full py-2 mb-4 px-4 sm:pr-0 rounded-input border-var-2 border-2 border-solid" individualInputAction={formHandler} inputClassnames="w-9/10 outline-none " inputName="email" inputPlaceholder="Write in your e-mail..." inputType="text" inputValidity={false} inputValue={""} isInSettingsPage={false} isPasswordField={false} isSelect={false} />
+                    <InputForForm smallDivClassnames="w-full h-full py-2 mb-4 px-4 sm:pr-0 rounded-input border-var-2 border-2 border-solid" individualInputAction={formHandler} inputClassnames="w-85 sm:w-9/10 outline-none " inputName="password" inputPlaceholder="Write in your password..." inputValidity={false} inputValue={""} isInSettingsPage={false} isPasswordField={true} isSelect={false} />
+                    {(textForSignInOrSignUpButton === "Sign up") && <InputForForm smallDivClassnames="w-full h-full py-2 mb-4 px-4 sm:pr-0 rounded-input border-var-2 border-2 border-solid" individualInputAction={formHandler} inputClassnames="w-85 sm:w-9/10 outline-none " inputName="confirmPassword" inputPlaceholder="Confirm your password..." inputValidity={false} inputValue={""} isInSettingsPage={false} isPasswordField={true} isSelect={false} />}
                     <button disabled={!stateOfForm.isFormValid} className="disabled:bg-black-inactive disabled:border-black-inactive disabled:cursor-pointer w-full py-2 px-2 mb-4 rounded-input bg-black text-var-1 border-black border-solid border-2 hover:bg-var-3 hover:border-var-3 duration-500 " onClick={signInOrSignUpHandle}>{textForSignInOrSignUpButton}</button>
                     <button className="flex flex-row justify-center items-center w-full py-2 px-2 mb-4 rounded-input bg-facebook text-var-1 hover:bg-facebook-hover duration-200 border-facebook border-solid border-2">
                         <FacebookSharpIcon className="mr-2"/>
@@ -130,6 +140,7 @@ export default function SignInOrSignUpWindow({ open, onClose, switchToSignIn, sw
                         Forgot your password?
                     </button>}
                 </div>
+ 
             </div>
         </div>
     )
