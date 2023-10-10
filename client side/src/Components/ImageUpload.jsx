ef621831fc171d1452397d9b127f653157610b70
Manuel Alanis <manuelaag99@gmail.com>
2023-10-09 20:37:42 -0600

    fix small bugs in imageupload

--- client side/src/Components/ImageUpload.jsx
+++ client side/src/Components/ImageUpload.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useRef, useState } from "react";
 
 import CloseIcon from '@mui/icons-material/Close';
+import InsertPhotoIcon from '@mui/icons-material/InsertPhoto';
 import { v4 as uuidv4 } from "uuid";
 
 export default function ImageUpload () {
@@ -36,16 +37,18 @@ export default function ImageUpload () {
 
     return (
         <div className="flex justify-center w-full h-full relative aspect-square">
-            <div className="absolute top-0 right-0 m-2 z-20">
+            {file && previewPhotoUrl && <div className="absolute top-0 right-0 m-2 z-20">
                 <button className="bg-black hover:bg-gray-600 duration-200 rounded-post px-2 text-white mr-3" onClick={(e) => selectFileHandler(e)}>
                     change
                 </button>
                 <button className="bg-black hover:bg-gray-600 duration-200 rounded-post px-2 text-white" onClick={cancelImageUpload}>
                     <CloseIcon />
                 </button>
-            </div>
-            {!file && !previewPhotoUrl && <button className="flex justify-center items-center w-full h-full z-10" onClick={(e) => selectFileHandler(e)}>
-                <p className="text-center">Select an image</p>
+            </div>}
+            {!file && !previewPhotoUrl && <button className="flex justify-center items-center w-full h-full z-10 cursor-pointer " onClick={(e) => selectFileHandler(e)}>
+                <p className="text-center cursor-pointer z-20 " onClick={(e) => selectFileHandler(e)}>
+                    <InsertPhotoIcon />  Select an image
+                </p>
             </button>}
             {file && previewPhotoUrl && <img alt="image" className="object-contain w-full z-10 cursor-pointer" src={previewPhotoUrl}  />}
             <input className="w-9/10 text-center hidden" onChange={(e) => uploadImage(e)} ref={imageSelectorRef} type="file" />
